41fac992941048742b57a8b0280da2058da6e0a3
memosize <89068024@qq.com>
2019-02-11 20:42:50 +0800

    redux sample demo

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import {connect} from 'react-redux'
 import {addGun,removeGun,addGunAsync} from './index.redux'
+  // 装饰器写法
+  // @connect(
+  //   state=>({num:state}),
+  //   { addGun, removeGun, addGunAsync}
+  // )
 class App extends Component {
-  
   render() { 
+    
     return (
       <div className="App">
        <h1>现在有机枪{this.props.num}把</h1>
        <button onClick={this.props.addGun}>申请武器</button>
        <button onClick={this.props.removeGun}>上交武器</button>
-       <button onClick={this.props.addGunAsync}>拖两天再给</button>
-
+       <button onClick={this.props.addGunAsync}>拖两天再给</button>   
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -7,14 +7,56 @@ import {createStore,applyMiddleware,compose} from 'redux'
 import thunk from 'redux-thunk'
 import {counter} from './index.redux'
 import {Provider} from 'react-redux'
+import {BrowserRouter,Route,Link,Redirect,Switch} from 'react-router-dom'
 const store = createStore(counter,compose(
     applyMiddleware(thunk),
     window.devToolsExtension?window.devToolsExtension():f=>f
 ))
-
+class Test extends React.Component{
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       
+    }
+  }
+    render(){
+      return(
+        <h2>测试组建：{this.props.match.params.location}</h2>
+      )
+    }
+  
+  
+}
+function Erying(){
+  return <h2>二营</h2>
+}
+function Qibinglian(){
+  return <h2>骑兵连</h2>
+}
 ReactDOM.render((
     <Provider store={store}>
-        <App />
+       <BrowserRouter>
+         <div>
+         <ul>
+           <li>
+             <Link to='/'>一营</Link>
+           </li>
+           <li>
+             <Link to='erying'>二营</Link>
+           </li>
+           <li>
+             <Link to='/qibinglian'>骑兵连</Link>
+           </li>
+         </ul>
+          <Switch>
+          <Route path='/' exact component={App}></Route>
+         <Route path='/erying' component={Erying}></Route>
+         <Route path='/qibinglian' component={Qibinglian}></Route>
+         <Route path='/:location' component={Test}/>
+          </Switch>
+         </div>
+       </BrowserRouter>
     </Provider>
 ), document.getElementById('root'));
 
